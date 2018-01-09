44f534d727541a41d2d4edd125e4d9e59dfdbc87
john bidwell <johnbidwellb@gmail.com>
2018-01-09 03:26:25 -0300

    Readme and setup files

---
+++ setup.py
@@ -0,0 +1,10 @@
+setup(
+    name = "azlyrics to pdf",
+    version = "0.0.1",
+    description = "Script that generates a PDF file of a song's lyric",
+    url = "https://github.com/JohnBidwellB/azlyrics-to-pdf",
+    author = "John Bidwell Boitano",
+    author_email = "johnbidwellb@gmail.com",
+    packages = ['azlyrics'],
+    install_requires = ['BeautifulSoup4', 'pdfkit'],
+)
